3f495020bce36572896c77211982068cca29b4bc
EnesCanbulat <enescanbulat35@gmail.com>
2026-09-06 21:54:26 +0300

    UserQuitProgress.cs güncel hali eklendi

---
+++ EasyPeasyAll/Models/Entities/UserQuitProgress.cs
@@ -0,0 +1,18 @@
+﻿namespace EasyPeasyAll.Models.Entities
+{
+    public class UserQuitProgress
+    {
+
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int AddictionId { get; set; }
+
+        public Addiction Addiction { get; set; }
+
+        public DateTime RecoveryDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
